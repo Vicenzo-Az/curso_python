33268e5b2458992910c52ccc35591e5c69c48354
Vicenzo-Az <vicenzo.net@gmail.com>
2025-03-24 15:55:40 -0300

    Add mais aulas

---
+++ aula18.py
@@ -0,0 +1,22 @@
+# if / elif      / else
+# se / se não se / se não
+condicao = False
+
+if condicao:
+    print('Este é o código do if')
+    print(1)
+else:
+    print('Esse é o do else')
+
+print('Fora do if')
+
+condicao1 = False
+condicao2 = True
+
+if condicao1:
+    print('cod 1')
+elif condicao2:
+    print('cod 2')
+else:
+    print('else')
+
