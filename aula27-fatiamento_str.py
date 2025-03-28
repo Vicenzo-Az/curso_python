4c6c86072e325813bb6be377620c4a9a31ecab11
Vicenzo-Az <vicenzo.net@gmail.com>
2025-03-28 09:43:27 -0300

    Add mais aulas

--- aula27-fatiamento_str.py
+++ aula27-fatiamento_str.py
@@ -8,3 +8,10 @@ Obs.: a função len retorna a qtd
 de caracteres da str
 """
 variavel = 'Olá mundo'
+print(variavel[5:])
+print(variavel[0:5])
+
+print(len(variavel))
+print(variavel[5:len(variavel)])
+
+print(variavel[::-1])
---
+++ aula29-try_except.py
@@ -0,0 +1,10 @@
+"""
+Introdução ao try/except
+try -> tentar executar o código
+except -> ocorreu algum erroa ao tentar executar
+"""
+numero  = input('Vou dobrar o número que vc digitar: ')
+numeor = ...
+
+numero_float = float(numero)
+print(f'O dobro de {numero_float} é {numero_float * 2:.2f}')
