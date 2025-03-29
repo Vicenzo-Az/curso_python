e96ed1442cd4648a559b57f3d63a6bc2c5bcd0c4
Vicenzo-Az <vicenzo.net@gmail.com>
2025-03-29 14:16:30 -0300

    Add aulas

--- section3-poo/aula121-metodos_inst.py
+++ section3-poo/aula121-metodos_inst.py
@@ -1,7 +1,23 @@
 # Métodos em instâncias de classes Python
-class Carro:
-    def __init__(self, alguma_coisa='sei lá'):
-        self.nome = alguma_coisa
 
-fusca = Carro()
+# class Carro:
+#     def __init__(self, alguma_coisa='sei lá'):
+#         self.nome = alguma_coisa
+
+# fusca = Carro()
+# print(fusca.nome)
+
+class Carro:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def acelerar(self):
+        print(f'{self.nome} está acelerando...')
+
+fusca = Carro('fusca')
 print(fusca.nome)
+fusca.acelerar()
+
+celta = Carro('celta')
+print(celta.nome)
+celta.acelerar()
