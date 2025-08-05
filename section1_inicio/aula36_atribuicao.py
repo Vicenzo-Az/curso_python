dbffb46d8d5b45decd1104f9af6b4bc4fcce1912
Vicenzo-Az <vicenzo.net@gmail.com>
2025-08-05 00:18:37 -0300

    feat: adicionar mais aulas

---
+++ section1_inicio/aula36_atribuicao.py
@@ -0,0 +1,26 @@
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+contador = 1
+
+contador += 5
+print(contador)
+
+contador -= 1
+print(contador)
+
+contador *= 10
+print(contador)
+
+contador /= 2
+print(contador)
+
+contador **= 2
+print(contador)
+
+contador //= 2
+print(contador)
+
+contador %= 1
+print(contador)
